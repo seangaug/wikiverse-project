c5ae6e24d8fc4769e9a50f6d487949d32d34c576
seangaug <seanjnr1991@aol.com>
2023-03-03 16:15:49 +0000

    initial fetch request and state added

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -8,11 +8,25 @@ export const App = () => {
 
 	const [pages, setPages] = useState([]);
 
+	//- Set the article data on state (a new piece of state)
+	const [page, setPage] = useState();
+
 	async function fetchPages(){
 		try {
 			const response = await fetch(`${apiURL}/wiki`);
 			const pagesData = await response.json();
 			setPages(pagesData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
+	// - Make a fetch request to the /wiki/:slug endpoint for the specific article.
+	async function fetchPage(slug){
+		try {
+			const response = await fetch(`${apiURL}/wiki/${slug}`);
+			const pageData = await response.json();
+			setPage(pageData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
